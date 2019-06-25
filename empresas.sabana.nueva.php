40adda326dfabee2c41132eb04b1ac8f8e05c99d
tanomartin <tanomartin05@gmail.com>
2019-06-25 12:27:31 -0300

    subida sabana nueva

--- empresas.sabana.nueva.php
+++ empresas.sabana.nueva.php
@@ -9,7 +9,7 @@ $delcod = $rowEmpre['delcod'];
 $anoactual =  date("Y");
 $mesacutal = date("m");
 	
-$anoinicio = $anoactual-10;
+$anoinicio = $anoactual-5;
 $mesinicio = $mesacutal+1;
 if($mesinicio == 13) {
 	$mesinicio = 1;
@@ -21,17 +21,24 @@ $anofin = $anoactual;
 
 function encuentroPagos($db) {
 	global $nrcuit, $anoinicio, $mesinicio, $anofin, $mesfin, $delcod;
-	$sqlPagos = "select anotra, mestra from pagos where nrcuit = $nrcuit and ((anotra > $anoinicio and anotra <= $anofin) or (anotra = $anoinicio and mestra >= $mesinicio))";
+	$sqlPagos = "select anotra, mestra, contra from pagos where nrcuit = $nrcuit and (contra = '381' or contra = '401') and ((anotra > $anoinicio and anotra <= $anofin) or (anotra = $anoinicio and mestra >= $mesinicio))";
 	//echo($sqlPagos."<br><br>");
 	$resPagos = mysql_query($sqlPagos,$db);
 	$CantPagos = mysql_num_rows($resPagos);
-	$arrayPagos = array();
+	$arrayConc = array();
 	if($CantPagos > 0) {
 		while ($rowPagos = mysql_fetch_assoc($resPagos)) {
 			$id=$rowPagos['anotra'].$rowPagos['mestra'];
-			$arrayPagos[$id] = array('anio' => $rowPagos['anotra'], 'mes' => $rowPagos['mestra']);
+			$arrayConc[$id][$rowPagos['contra']] = array('anio' => $rowPagos['anotra'], 'mes' => $rowPagos['mestra']);	
 		}
 	} 
+	$arrayPagos = array();
+	foreach($arrayConc as $concepto) {
+		if (isset($concepto['401']) && isset($concepto['381'])) {
+			$id = $concepto['401']['anio'].$concepto['401']['mes'];
+			$arrayPagos[$id] = array('anio' => $concepto['401']['anio'], 'mes' => $concepto['401']['mes']);
+		}
+	}
 	return($arrayPagos);
 }
 
@@ -83,6 +90,22 @@ function encuentroDdjj($db) {
 	return($arrayDdjj);
 }
 
+function encuentroReque($db) {
+	global $nrcuit, $anoinicio, $mesinicio, $anofin, $mesfin, $delcod;
+	$sqlReque = "select * from requerimientos where nrcuit = $nrcuit and ((anofis > $anoinicio and anofis <= $anofin) or (anofis = $anoinicio and mesfis >= $mesinicio))";
+	//echo($sqlReque."<br><br>");
+	$resReque = mysql_query($sqlReque,$db);
+	$canReque = mysql_num_rows($resReque);
+	$arrayReque = array();
+	if($canReque > 0) {
+		while ($rowReque = mysql_fetch_assoc($resReque)) {
+			$id=$rowReque['anofis'].$rowReque['mesfis'];
+			$arrayReque[$id] = $rowReque['nroreq'];
+		}
+	}
+	return($arrayReque);
+}
+
 
 $arrayPagos = encuentroPagos($db);
 //var_dump($arrayPagos);echo"<br><br>";
@@ -92,10 +115,12 @@ $arrayAcuerdos = encuentroAcuerdos($db);
 //var_dump($arrayAcuerdos);echo"<br><br>";
 $arrayDdjj = encuentroDdjj($db);
 //var_dump($arrayDdjj);echo"<br><br>";
+$arrayReque = encuentroReque($db);
+//var_dump($arrayReque);echo"<br><br>";
 
 function estado($ano, $me) {
 	global $cuit, $anoinicio, $mesinicio, $anofin, $mesfin;
-	global $arrayPagos, $arrayAcuerdos, $arrayJuicios, $arrayDdjj;
+	global $arrayPagos, $arrayAcuerdos, $arrayJuicios, $arrayDdjj, $arrayReque;
 	//VEO QUE EL MES Y EL AÑO ESTEND DENTRO DE LOS PERIODOS A MOSTRAR
 	if ($ano == $anoinicio) {
 		if ($me < $mesinicio) {
@@ -121,13 +146,17 @@ function estado($ano, $me) {
 			if (array_key_exists($idArray, $arrayJuicios)) {
 				$des = "JUICI.";
 			} else {
-				// VEO LAS DDJJ REALIZADAS SIN PAGOS
-				if(array_key_exists($idArray, $arrayDdjj)) {
-					$des = "NO PAGO";
+				//VEO LOS REQUE
+				if (array_key_exists($idArray, $arrayReque)) {
+					$des = "REQUE. ($arrayReque[$idArray])";
 				} else {
-					// NO HAY DDJJ SIN PAGOS
-					$des = "S.DJ.";
-				} //else DDJJ
+					// VEO LAS DDJJ REALIZADAS SIN PAGOS
+					if(array_key_exists($idArray, $arrayDdjj)) {
+						$des = "NO PAGO";
+					} else {
+						$des = "S.DJ.";
+					} //else DDJJ
+				} //else REQUE
 			} //else JUICIOS
 		}//else ACUERDOS
 	}
@@ -216,7 +245,7 @@ function estado($ano, $me) {
 										if ($descri == "NO PAGO") { ?>
 											<td><a href="javascript:mypopup('empresas.sabana.ddjj.php?nrcuit=<?php echo $nrcuit ?>&ano=<?php echo $ano ?>&mes=<?php echo $mes ?>')"><?php echo $descri ?></a></td>
 						<?php			}
-										if (($descri == "JUICI.")|| ($descri == "S.DJ.")) { ?>
+										if (($descri == "JUICI.") || strpos($descri, "REQUE.") !== false || ($descri == "S.DJ.")) { ?>
 											<td><?php echo $descri ?></td>
 						<?php			}
 									}
@@ -227,16 +256,16 @@ function estado($ano, $me) {
 					<?php } ?>
 					</tbody>
 				</table>
-				<table class="table">
+				<table class="table" style="text-align: center">
 				  <tr>
-				    <td><div align="left"><b>*ACUER.</b> = PERIODO EN ACUERDO </div></td>
-				    <td><div align="center"><b>*S. DJ.</b>= PERIODO SIN DDJJ </div></td>
-				    <td><div align="right"><b>*NO PAGO</b> = PERIODO NO PAGO CON DDJJ</div></td>
+				    <td style="width: 33%"><b>*ACUER.</b><br> PERIODO EN ACUERDO</td>
+				    <td style="width: 33%"><b>*S. DJ.</b><br> PERIODO SIN DDJJ</td>
+				    <td style="width: 33%"><b>*JUICI.</b><br> PERIODO EN JUICIO</td>
 				  </tr>
 				  <tr>
-				    <td><div align="left"><b>*JUICI.</b> = PERIODO EN JUICIO</div></td>
-				    <td><div align="center"><b>*PAGO</b> = PERIODO PAGO CON DDJJ</div></td>
-				    <td></td>
+				  	<td><b>*NO PAGO</b><br> PERIODO NO PAGO CON DDJJ</td>
+				    <td><b>*REQUE. (#)</b><br> PERIODO EN FISCALIZACION</td> 
+				   	<td><b>*PAGO</b><br> PERIODO PAGO CON DDJJ</td>
 				  </tr>
 				</table>
 				<a class="nover" href="javascript:window.print();"><i title="Imprimir" style="font-size: 40px; margin-bottom: 20px"  class="glyphicon glyphicon-print"></i></a>
