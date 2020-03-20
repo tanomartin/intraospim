d890adbe30dd4e944d09250eaa3b024ff7791a3a
tanomartin <tanomartin05@gmail.com>
2020-03-20 14:42:39 -0300

    subida 1er comunicado coronavirus subida division de pantallas coronavirus

--- coronavirus.php
+++ coronavirus.php
@@ -22,6 +22,42 @@
 				<i style="font-size: 50px;color: red" class="glyphicon glyphicon-asterisk"></i><br>CORONAVIRUS
 			</h2>
 			<h3 style="color: blue">CANAL DE COMUNICACION INSTITUCIONAL</h3>
+			<div class="col-md-10 col-md-offset-1">
+				<div class="panel panel-default text-center">
+        			<p style="text-align: justify; margin: 15px">Ante la realidad que nos toca vivir, la U.S.I.M.R.A. respondiendo a decisiones gubernamentales, limita la actividad social y gremial a comunicaciones electrónicas, extendiendo la moción a todos los Sindicatos  y Delegaciones adheridos.<br> 
+        			   Se habilitará un canal de comunicación a través de las intranets de U.S.I.M.R.A. y O.S.P.I.M. a los efectos de centralizar la informacion que deba ser comunicada. <br>
+        			   El Dpto. de Sistemas está elaborando estrategias y estableciendo configuraciones que oportunamente seran informadas.</p>
+         			<p style="text-align: right; margin: 15px"><b>Secretario General U.S.I.M.R.A.</b></p>
+				</div>
+			</div>
+			<div class="col-md-4 col-md-offset-1">
+					<div class="panel panel-default text-center">
+						<div class="panel-heading">
+							<h3 class="panel-title">Comunicaciones</h3>
+						</div>
+						<div class="panel-body">
+							<i style="font-size: 100px"  class="glyphicon glyphicon-info-sign"></i>
+						</div>
+						<ul class="list-group">
+							<li class="list-group-item"><a href="coronavirus.comunicacion.php" class="btn btn-primary">Ingresar</a></li>
+						</ul>
+					</div>
+				</div>
+			 	<div class="col-md-4 col-md-offset-2">
+					<div class="panel panel-default text-center">
+						<div class="panel-heading">
+							<h3 class="panel-title">Contenido</h3>
+						</div>
+						<div class="panel-body">
+							<i style="font-size: 100px"  class="glyphicon glyphicon-tasks"></i>
+						</div>
+						<ul class="list-group">
+							<li class="list-group-item"><a href="coronavirus.contenido.php" class="btn btn-primary">Ingresar</a></li>
+						</ul>
+					</div>
+				</div> 
+			
+			
 			<div class="col-md-12 panel-footer">
 				<?php  print ("&Uacute;LTIMA ACTUALIZACI&Oacute;N - " . $_SESSION['fecult']); ?>
 				<p>&copy; 2016 O.S.P.I.M.<p>
